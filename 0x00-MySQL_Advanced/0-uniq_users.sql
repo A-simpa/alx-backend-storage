3aded7b9f486e6db66d6d0a79dcc54e3a8826b1f
A-simpa <mohammedabdulmujeeb16@gmail.com>
2023-01-12 13:08:18 +0000

    new: added 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- create a table called users
 -- with an primary auto increment primary key id
 
-CREATE TABLE `users`(
+CREATE TABLE IF NOT EXISTS `users`(
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`email` VARCHAR(255) UNIQUE NOT NULL,
 	`name` VARCHAR(255),
