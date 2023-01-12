a6f61bd2fbe9b550de9ec8f4ee3f5aa3e3631bb2
A-simpa <mohammedabdulmujeeb16@gmail.com>
2023-01-12 13:31:00 +0000

    new: added 1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- create a table called users
+-- with an primary auto increment primary key id
+-- having a default colum country which can be US, CO, TN
+
+CREATE TABLE IF NOT EXISTS `users`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`email` VARCHAR(255) UNIQUE NOT NULL,
+	`name` VARCHAR(255),
+	`country` ENUM('US', 'CO', 'TN') DEFAULT('US'),
+	PRIMARY KEY(id));
