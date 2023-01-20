d103982f201b1b527b1570d3d1ee4543daf85e03
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-20 21:19:14 +0100

    new: let add 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,14 +3,16 @@
 printing out each request method in logs"""
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client["logs"]
-print("{} logs".format(db.nginx.count_documents({})))
-print("Methods:")
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for med in methods:
-    criteria = {'method': med}
-    print(
-        "\tmethod {}: {}".format(med, db.nginx.count_documents(criteria)))
-criteria = {'method': 'GET', 'path': '/status'}
-print("{} status check".format(db.nginx.count_documents(criteria)))
+
+if __name__ == '__main__':
+    client = MongoClient()
+    db = client["logs"]
+    print("{} logs".format(db.nginx.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for med in methods:
+        criteria = {'method': med}
+        print(
+            "\tmethod {}: {}".format(med, db.nginx.count_documents(criteria)))
+    criteria = {'method': 'GET', 'path': '/status'}
+    print("{} status check".format(db.nginx.count_documents(criteria)))
