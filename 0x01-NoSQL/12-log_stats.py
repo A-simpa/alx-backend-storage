ab6e1be6baa9ef03f6dc9a2f2d190ae2e447e27a
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-20 20:52:28 +0100

    new: 12-log_stats.py

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client["logs"]
+print(f"{db.nginx.count_documents({})} logs")
+print(f"Methods:")
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+for med in methods:
+    print(f"\tmethod {med}: {db.nginx.count_documents({'method': med})}")
+criteria = {'method': 'GET', 'path': '/status'}
+print(f"{db.nginx.count_documents(criteria)} status check")
