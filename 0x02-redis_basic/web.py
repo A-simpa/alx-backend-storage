60135b33c1c4deb04d4cfb546f83e88091a3d147
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-28 19:02:28 +0100

    refactor: added new functionality replay

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,7 +7,7 @@ import redis
 def get_page(url: str) -> str:
     """request a page"""
     db = redis.Redis()
-    db.incr("count:{}".format(url), 1)
-    db.expireat("count:{}".format(url), 10)
+    db.incr("count:{" + "{}".format(url) +"}", 1)
+    db.expireat("count:{" + "{}".format(url) + "}", 10)
     x = requests.get(url)
     return (x.text)
